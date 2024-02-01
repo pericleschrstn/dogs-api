a732f2f63b236cefa5b6ff0bd8da410e4f433f0e
Pericles Christian <pericleschrstn@gmail.com>
2024-02-01 15:00:46 -0300

    feat: create user post and jwt auth

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+// Remove as rotas definidas pelo WordPress
+remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+
+$dirbase = get_template_directory();
+require_once $dirbase . '/endpoints/user_post.php';
+
+// Modifica o prefixo da API de wp-json para json apenas
+// Necessário salvar os permalinks para dar um refresh nos URL's
+function change_api() {
+  return 'json';
+}
+
+add_filter('rest_url_prefix', 'change_api');
+
+function expire_token() {
+  return time() + (60 * 60 * 24)
+}
+
+add_action('jwt_auth_expire', 'expire_token')
+?>
