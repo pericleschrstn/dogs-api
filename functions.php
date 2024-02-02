c65553e1351d5b8e14aeff7279b0ea6d47a1ba94
Pericles Christian <pericleschrstn@gmail.com>
2024-02-02 12:42:24 -0300

    feat: endpoint photo post

--- functions.php
+++ functions.php
@@ -1,11 +1,18 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 // Remove as rotas definidas pelo WordPress
-remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+// remove_action('rest_api_init', 'create_initial_rest_routes', 99);
 
 $dirbase = get_template_directory();
 require_once $dirbase . '/endpoints/user_post.php';
 require_once $dirbase . '/endpoints/user_get.php';
+
+require_once $dirbase . '/endpoints/photo_post.php';
+
+update_option('large_size_w', 1000);
+update_option('large_size_h', 1000);
+update_option('large_crop', 1);
+
 
 // Modifica o prefixo da API de wp-json para json apenas
 // Necessário salvar os permalinks para dar um refresh nos URL's
