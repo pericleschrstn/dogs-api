abe2885e96659f2c00ea751fbc4315feab6d125a
Pericles Christian <pericleschrstn@gmail.com>
2024-02-02 10:09:22 -0300

    feat: endpoint user get

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ remove_action('rest_api_init', 'create_initial_rest_routes', 99);
 
 $dirbase = get_template_directory();
 require_once $dirbase . '/endpoints/user_post.php';
+require_once $dirbase . '/endpoints/user_get.php';
 
 // Modifica o prefixo da API de wp-json para json apenas
 // Necessário salvar os permalinks para dar um refresh nos URL's
@@ -15,7 +16,7 @@ function change_api() {
 add_filter('rest_url_prefix', 'change_api');
 
 function expire_token() {
-  return time() + (60 * 60 * 24)
+  return time() + (60 * 60 * 24);
 }
 
 add_action('jwt_auth_expire', 'expire_token')
