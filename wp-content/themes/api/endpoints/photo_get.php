1aba166ae5ce71767e434a75249e67afd94253f0
Pericles Christian <pericleschrstn@gmail.com>
2024-02-08 13:54:15 -0300

    feat: photos get

--- wp-content/themes/api/endpoints/photo_get.php
+++ wp-content/themes/api/endpoints/photo_get.php
@@ -54,8 +54,37 @@ add_action('rest_api_init', 'register_api_photo_get');
 
 // Puxa uma lista de fotos
 function api_photos_get($request) {
-  $response = 'teste';
-  return rest_ensure_response($response);
+  $_total = sanitize_text_field($request['_total']) ?: 6;
+  $_page = sanitize_text_field($request['_page']) ?: 1;
+  $_user = sanitize_text_field($request['_user']) ?: 0;
+
+  if (!is_numeric($_user)) {
+    $user = get_user_by('login', $_user);
+    if (!$user) {
+      $response = new WP_Error('error', 'Usuário não encontrado', ['status' => 404]);
+      return rest_ensure_response($response);
+    }
+    $_user = $user->ID;
+  }
+
+  $args = [
+    'post_type' => 'post',
+    'author' => $_user,
+    'posts_per_page' => $_total,
+    'paged' => $_page,
+  ];
+
+  $query = new WP_Query($args);
+  $posts = $query->posts;
+
+  $photos = [];
+  if ($posts) {
+    foreach ($posts as $post) {
+      $photos[] = photo_data($post);
+    }
+  }
+
+  return rest_ensure_response($photos);
 }
 
 function register_api_photos_get() {
